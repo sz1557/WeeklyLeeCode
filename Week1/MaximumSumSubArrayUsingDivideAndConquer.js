0f487a82a5f8ecb7edffc8919ba811dced5a94a6
sz1557 <zscjswhk@gmail.com>
2022-09-07 13:36:14 -0500

    update

--- Week1/MaximumSumSubArrayUsingDivideAndConquer.js
+++ Week1/MaximumSumSubArrayUsingDivideAndConquer.js
@@ -1,15 +1,52 @@
 let arr = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
 
-const maxSubArray = function(nums) {
-    let prev = 0;
-    let total = -Infinity;
+// const maxSubArray = (nums) => {
+//     let prev = 0;
+//     let total = -Infinity;
     
-    for(let i = 0; i<nums.length; i++) {
-        prev = Math.max((prev+nums[i]), nums[i])
-        total = Math.max(total, prev);
-    }
-    console.log(total);
-    return total;
+//     for(let i = 0; i<nums.length; i++) {
+//         prev = Math.max((prev+nums[i]), nums[i])
+//         total = Math.max(total, prev);
+//     }
+//     console.log(total);
+//     return total;
+// };
+
+// maxSubArray(arr);
+
+// Divide and Conquer Solution
+
+var maxSubArray = function(nums) {
+    return findMax(nums, 0, nums.length - 1)
 };
 
-maxSubArray(arr);
+var findMax = function (nums, l, r) {
+    if (l == r) {
+        return nums[l]
+    }
+    
+    let mid = Math.floor((l + r)/2)
+    let leftSum = findMax(nums, l, mid) 
+    let rightSum = findMax(nums, mid+1, r) 
+    const crossingSum = findCrossingSum(nums, l, mid, r)
+    return Math.max (crossingSum, leftSum, rightSum)
+}
+
+var findCrossingSum = function(nums, l, mid, r) { 
+    let sum=0
+    let maxLSum = -Infinity
+    for(let i = mid; i >= l; i--) {
+        sum += nums[i]
+        maxLSum = Math.max(maxLSum, sum)
+    }
+    sum =0
+    let maxRSum = -Infinity
+    for (let i = mid + 1; i <= r; i++) {
+        sum += nums[i]
+        maxRSum = Math.max(maxRSum, sum)
+    }
+    
+    return maxRSum + maxLSum;
+}
+
+console.log(maxSubArray(arr));
